84ebe0f5201fc5c3349faf031e7e2e4b11702566
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2020-03-28 18:28:26 -0700

    Modify styles

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,7 @@ router.post('/registered', async (req, res) => {
 })
 
 // Log in existing user
-router.post('loggedin', async (req, res) => {
+router.post('/loggedin', async (req, res) => {
 	try {
 		const user = await User.findByCredentials(req.body.email, req.body.password)
 		const token = await user.generateAuthToken()
