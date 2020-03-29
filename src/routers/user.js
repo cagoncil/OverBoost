6e191d83d416271170750ed802e2886f22470b2f
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2020-03-28 17:49:16 -0700

    Modify styles

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,7 +14,8 @@ router.post('/users', async (req, res) => {
 		await user.save()
 		const token = await user.generateAuthToken()
 		res.cookie('auth_token', token)
-		res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+		//res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+		res.sendFile(path.resolve(__dirname, '..', '..', 'public', 'index.html'))
 	} catch (e) {
 		res.status(400).send(e)
 	}
