1f6b5a81b6bd5cb705c908bde449f7463a054f39
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2020-04-07 19:02:51 -0700

    Try to fix logout issue

--- src/routers/user.js
+++ src/routers/user.js
@@ -54,14 +54,14 @@ router.post('/logout', auth, async (req, res) => {
 
 // ===== Read =====
 
-// Go back to dashboard
-// router.get('/dashboard', auth, (req, res) => {
-// 	try {
-// 		res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
-// 	} catch (e) {
-// 		res.status(500).send()
-// 	}
-// })
+// Reach dashboard after login or registration / Return to dashboard
+router.get('/dashboard', auth, (req, res) => {
+	try {
+		res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+	} catch (e) {
+		res.status(500).send()
+	}
+})
 
 // Get user profile
 router.get('/profile', auth, (req, res) => {
