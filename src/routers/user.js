fa2294707b62e69bb683fb251938c9474919a4d0
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2020-04-07 19:21:11 -0700

    Try to fix logout issue

--- src/routers/user.js
+++ src/routers/user.js
@@ -14,7 +14,10 @@ router.post('/welcome', async (req, res) => {
 		await user.save()
 		const token = await user.generateAuthToken()
 		res.cookie('auth_token', token)
-		res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+		// res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+
+		// Instead of sendFile, redirect to a GET request for /dashboard to prevent resubmission of sensitive info
+		res.redirect('/dashboard')
 	} catch (e) {
 		res.status(400).send(e)
 	}
@@ -27,10 +30,10 @@ router.post('/dashboard', async (req, res) => { // use one word instead of two (
 		const token = await user.generateAuthToken()
 		res.cookie('auth_token', token)
 		// res.header('Cache-Control', 'no-cache, max-age=0, stale-while-revalidate=300')
-		// res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
+		 res.sendFile(path.resolve(__dirname, '..', 'views', 'dashboard.html'))
 		
 		// Instead of sendFile, redirect to a GET request for /dashboard to prevent resubmission of sensitive info
-		res.redirect('/dashboard')
+		// res.redirect('/dashboard')
 	} catch (e) {
 		 res.status(400).send()
 		// res.status(400).redirect('/')
