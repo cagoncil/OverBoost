9a3200d1f0559f57c96488cd9eb88f2384154c15
Crystal Agoncillo <crystal.agoncillo@gmail.com>
2020-05-11 19:13:09 -0700

    Integrate email sending functionality

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useCreateIndex: true,
